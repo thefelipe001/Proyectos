164ecd5e4879a359fa6aa067708faba0ccecd9cf
thefelipe001 <elfelipe_lugo@hotmail.com>
2021-09-26 09:59:28 -0400

    Guardar Cambios

--- Create Database and Table.sql
+++ Create Database and Table.sql
@@ -1,1 +1,11 @@
 create database DB_Tienda
+Go
+USE DB_Tienda
+go
+
+CREATE TABLE CATEGORIA(
+IdCategoria int primary key identity,
+Descripcion varchar(100),
+Activo bit default 1,
+FechaRegistro datetime default getdate()
+)
